ebc3599acc93ac6c935e0433d60a587616915016
mishael254 <mishaelmishantoz@gmail.com>
2022-07-22 12:46:33 +0300

    increment the value found in the object after showing its output and the loop starts again this timewith an incremented value

--- edsac_program/edsac.cpp
+++ edsac_program/edsac.cpp
@@ -8,6 +8,7 @@ int main() {
 	cout << "The following program  works how edsac did work\nTo print digits with its squares\n " << endl<<"digit    digit square\n";
 
 	int digit = 0;
+
 	while (digit<100) {
 
 
